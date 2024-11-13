49cde13b6b9233d033ad484399515b710b0dc1a9
Agustin Arroyo <agusarroyolaurito@gmail.com>
2024-11-12 21:29:56 -0300

    modificando el services

--- src/services/services.js
+++ src/services/services.js
@@ -1,72 +1,61 @@
-const { model } = require("mongoose");
-const BookModel = require("../models/book.model");
+const Book = require('../models/book.model');
 
-const addBookService = async (req, res) => {
-  const book = req.body;
+const addBookService = async (book) => {
   try {
-    const newBook = new BookModel(book);
-
+    const newBook = new Book(book);
     await newBook.save();
-
     return { message: "Libro generado satisfactoriamente", statusCode: 201 };
   } catch (error) {
-    return { message: "Ocurrió un error", statusCode: 400 };
+    throw new Error('Ocurrió un error al agregar el libro');
   }
 };
 
-const readAllBookService = async (req, res) => {
-  const allBooks = await BookModel.find(req);
-
-  return allBooks;
-};
-
-const getBookByIdService = async (req, res) => {
-  const { id } = req.params;
-
-  const bookById = await BookModel.findById(id);
-
-  if (!bookById) {
-    return { message: "Libro no encontrado", statusCode: 404 };
-  }
-
-  return bookById;
-};
-
-const updateBookService = async (req, res) => {
-  const { id } = req.params;
-  const carToEdit = req.body;
-
+const readAllBookService = async () => {
   try {
-    const bookById = await BookModel.findById(id);
+    const allBooks = await Book.find();
+    return allBooks;
+  } catch (error) {
+    throw new Error('Ocurrió un error al obtener los libros');
+  }
+};
 
+const getBookByIdService = async (id) => {
+  try {
+    const bookById = await Book.findById(id);
     if (!bookById) {
       return { message: "Libro no encontrado", statusCode: 404 };
     }
-
-    bookById.name = carToEdit.name;
-    bookById.price = carToEdit.price;
-    bookById.description = carToEdit.description;
-
-    await bookById.save();
-
-    return { message: "Libro actualizado con éxito", statusCode: 200 };
+    return bookById;
   } catch (error) {
-    return { message: "Ocurrió un error", statusCode: 400 };
+    throw new Error('Ocurrió un error al obtener el libro por ID');
   }
 };
 
-const deleteBookService = async (req, res) => {
-  const { id } = req.params;
-
+const updateBookService = async (id, bookData) => {
   try {
-    const bookToDelete = await BookModel.deleteOne({ _id: id });
+    const bookById = await Book.findById(id);
+    if (!bookById) {
+      return { message: "Libro no encontrado", statusCode: 404 };
+    }
+    bookById.name = bookData.name;
+    bookById.price = bookData.price;
+    bookById.description = bookData.description;
+    await bookById.save();
+    return { message: "Libro actualizado con éxito", statusCode: 200 };
+  } catch (error) {
+    throw new Error('Ocurrió un error al actualizar el libro');
+  }
+};
 
+const deleteBookService = async (id) => {
+  try {
+    const bookToDelete = await Book.deleteOne({ _id: id });
     if (bookToDelete.deletedCount === 0) {
       return { message: "Libro no encontrado", statusCode: 404 };
     }
     return { message: "Libro eliminado con éxito", statusCode: 200 };
   } catch (error) {
-    return { message: "Ocurrió un error", statusCode: 400 };
+    throw new Error('Ocurrió un error al eliminar el libro');
   }
 };
 
